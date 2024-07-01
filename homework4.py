a26b0b7ab3ea19d8caf20ef133faf1e458ea1656
Кахбек Халилов <profjeriko@outlook.com>
2024-07-01 18:29:50 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input("Из какого вы города: ")
+print(my_string)
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ", ""))
+front_symbol = my_string[0]
+print(front_symbol)
+last_symbol = my_string[-1]
+print(last_symbol)
